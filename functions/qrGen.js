47352acdd26eff3247de741143425d50c92ce20e
Glitch (chisel-lavender-intelligence) <none>
2025-03-26 11:14:53 +0000

    🚋🐓 Checkpoint ./functions/qrGen.js:40274427/1319

--- functions/qrGen.js
+++ functions/qrGen.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 function crc16Ccitt(data) {
   let crc = 0xFFFF;
   for (let i = 0; i < data.length; i++) {
@@ -31,6 +33,17 @@ module.exports = {
   generateQr: async function(amount) {
     let qrData = "00020101021227830012com.p2pqrpay0111GXCHPHM2XXX02089996440303152170200000006560417DWQM4TK3JDO5YAWDV5204601653036085406"+amount+".005802PH5912I** PA**O I.6011San Antonio610412346304"
     let generatedQr = generateQrCRC(qrData)
+    
+    let data = {
+      method: 'POST',
+      body: JSON.stringify({"data":qrData,"config":{"body":"square","eye":"frame0","eyeBall":"ball0","erf1":[],"erf2":[],"erf3":[],"brf1":[],"brf2":[],"brf3":[],"bodyColor":"#000000","bgColor":"#FFFFFF","eye1Color":"#000000","eye2Color":"#000000","eye3Color":"#000000","eyeBall1Color":"#000000","eyeBall2Color":"#000000","eyeBall3Color":"#000000","gradientColor1":"","gradientColor2":"","gradientType":"linear","gradientOnEyes":"true","logo":"","logoMode":"default"},"size":1000,"download":"imageUrl","file":"svg"}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    let qrCode = await fetch("https://api.qrcode-monkey.com//qr/custom",data)
+    qrCode = await qrCode.json()
+    
     return generatedQr;
   }
 };

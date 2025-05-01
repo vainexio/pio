726d79edf6116c5ddc423988f1d3af605ed1dc52
Glitch (chisel-lavender-intelligence) <none>
2025-05-01 16:23:01 +0000

    🕰🌍 Checkpoint ./storage/settings_.js:40274427/2

--- storage/settings_.js
+++ storage/settings_.js
@@ -813,7 +813,7 @@ module.exports = {
               { name: '800 robux', price: 264, rs: 260 },
               { name: '900 robux', price: 297, rs: 293 },
               { name: '1000 robux', price: 330, rs: 325 },
-              { name: '*covered tax (hrs-2d process)*', price: 0, rs: 0 },
+              { name: '*covered tax (hrs-3d process)*', price: 0, rs: 0 },
               //
             ],
           },

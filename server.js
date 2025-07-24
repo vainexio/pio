1387c3bbc1c0f9bc30688c9b117e11f3315045df
vai <111634400+vainexio@users.noreply.github.com>
2025-07-24 18:55:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3086,10 +3086,7 @@ client.on('interactionCreate', async inter => {
         if (booster) price = totalAmount * .255
         else price = totalAmount * .260
       }
-      else if (
-        (item.includes("robux") || item.includes("gamepass")) || item.includes("gpass"))
-        && typeof amount === "string"
-      ) { //
+      else if ((item.includes("robux") || item.includes("gamepass") || item.includes("gpass")) && typeof amount === "string") { //
         // 1) parse the string into an array of numeric amounts:
         let amounts = parseRobuxAmounts(thread[1].answer);
         let totalAmount = amounts.reduce((sum, a) => sum + a, 0);
